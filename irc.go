1fe461faff6f17e91daddd358f1fd7a26968ec0a
Adrian Gallagher <thrasher@addictionsoftware.com>
2014-09-03 13:31:09 +1000

    - Added irc.go. - Added support for sending messages to channel. - Removed useless cfg struct - IRC raw send is now a variadic function.

---
+++ irc.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strings"
+	"strconv"
+)
+
+type IRC struct {
+	server, nickname, username, channel string
+	testconn net.Conn
+	connected bool
+}
+
+func (i *IRC) SendToChannel(data string, v ...interface{}) {
+	s := fmt.Sprintf("PRIVMSG %s :%s", i.channel, data)
+	buffer := fmt.Sprintf(s, v...)
+	_, err := i.testconn.Write([]byte(buffer))
+
+	if err != nil {
+		fmt.Println("Error, unable to send data.")
+		i.connected = false
+	}
+
+	data = data[:len(data)-2]
+	fmt.Printf("Sending channel message: %s\n", data)
+}
+
+func (i *IRC) WriteData(data string, v ...interface{}) {
+	buffer := fmt.Sprintf(data, v...)
+	_, err := i.testconn.Write([]byte(buffer))
+
+	if err != nil {
+		fmt.Println("Error, unable to send data.")
+		i.connected = false
+	}
+
+	buffer = buffer[:len(buffer)-2]
+	fmt.Printf("Sending: %s\n", buffer)
+}
+
+func (i *IRC) CloseConnection() {
+	i.testconn.Close();
+}
+
+func (i *IRC) RecvData() {
+	var completedBuffer = ""
+
+	for {
+		readBuffer := make([]byte, 1);
+		_, err := i.testconn.Read(readBuffer);
+
+		if err != nil {
+			fmt.Println("Error, unable to receive data.")
+			i.connected = false
+			return;
+		}
+
+		s := string(readBuffer);
+
+		if strings.Contains(completedBuffer, "\r") || strings.Contains(completedBuffer, "\n") {
+			break;
+		} else {
+			completedBuffer += s
+		}
+
+		if err != nil {
+			fmt.Println("Error, unable to recv data.")
+		}
+	}
+		
+	fmt.Printf("Received: %s\n", completedBuffer);
+	i.HandleIRCEvents(completedBuffer)
+}
+
+func (i *IRC) ConnectToServer() bool { 
+	ports := strings.Split(i.server, ":")[1]
+	port, err := strconv.Atoi(ports)
+
+	if err != nil {
+		return false
+	}
+	if !((port >= 0) && (port <= 65535)) {
+		return false
+	}
+
+	fmt.Printf("Attempting connection to %s..\n", i.server)
+	i.testconn, err = net.Dial("tcp", i.server)
+	
+	if err != nil {
+		fmt.Println("Error, unable to connect.")
+		return false
+	}
+
+	i.connected = true
+	return true
+}
+
+func (i *IRC) HandleIRCEvents(data string) {
+	if (strings.Contains(data, "001")) {
+		i.WriteData("JOIN %s\r\n", i.channel)
+	} else if (strings.Contains(data, "433")) {
+		i.nickname = i.nickname + "`"
+		i.WriteData("NICK %s\r\n", i.nickname)
+	} else if (strings.Contains(data, "PING")) {
+		s := strings.Split(data, " ")[1]
+		i.WriteData("PONG %s\r\n", s)
+	} else if (strings.Contains(data, "SECRETTEST")) {
+		i.SendToChannel("test123\r\n");
+	}
+	return;
+}
--- main.go
+++ main.go
@@ -1,120 +1,20 @@
-package main
+package main 
 
-import "fmt"
-import "net"
-import "time"
-import "strings"
-import "strconv"
-
-//testing commit
-
-type Config struct {
-	IRCServer, Nickname, Username, Channel string
-}
-
-type IRC struct {
-	server, nickname, username, channel string
-	testconn net.Conn
-	connected bool
-}
-
-func (i *IRC) WriteData(data string, v ...interface{}) {
-	buffer := fmt.Sprintf(data, v...)
-	_, err := i.testconn.Write([]byte(buffer))
-
-	if err != nil {
-		fmt.Println("Error, unable to send data.")
-		i.connected = false
-	}
-
-	buffer = buffer[:len(buffer)-2]
-	fmt.Printf("Sending: %s\n", buffer)
-}
-
-func (i *IRC) CloseConnection() {
-	i.testconn.Close();
-}
-
-func (i *IRC) RecvData() {
-	var completedBuffer = ""
-
-	for {
-		readBuffer := make([]byte, 1);
-		_, err := i.testconn.Read(readBuffer);
-
-		if err != nil {
-			fmt.Println("Error, unable to receive data.")
-			i.connected = false
-			return;
-		}
-
-		s := string(readBuffer);
-
-		if strings.Contains(completedBuffer, "\r") || strings.Contains(completedBuffer, "\n") {
-			break;
-		} else {
-			completedBuffer += s
-		}
-
-		if err != nil {
-			fmt.Println("Error, unable to recv data.")
-		}
-	}
-		
-	fmt.Printf("Received: %s\n", completedBuffer);
-	i.HandleIRCEvents(completedBuffer)
-}
-
-func (i *IRC) ConnectToServer() bool { 
-	ports := strings.Split(i.server, ":")[1]
-	port, err := strconv.Atoi(ports)
-
-	if err != nil {
-		return false
-	}
-	if !((port >= 0) && (port <= 65535)) {
-		return false
-	}
-
-	fmt.Printf("Attempting connection to %s..\n", i.server)
-	i.testconn, err = net.Dial("tcp", i.server)
-	
-	if err != nil {
-		fmt.Println("Error, unable to connect.")
-		return false
-	}
-
-	i.connected = true
-	return true
-}
-
-func (i *IRC) HandleIRCEvents(data string) {
-	if (strings.Contains(data, "001")) {
-		i.WriteData("JOIN %s\r\n", i.channel)
-	} else if (strings.Contains(data, "433")) {
-		i.nickname = i.nickname + "`"
-		i.WriteData("NICK %s\r\n", i.nickname)
-	} else if (strings.Contains(data, "PING")) {
-		s := strings.Split(data, " ")[1]
-		i.WriteData("PONG %s\r\n", s)
-	}
-	return;
-}
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	cfg := Config{"irc.freenode.net:6667", "PugBotTestAA", "0 0 0 0", "#PugBotTest"}
-	irc := IRC{cfg.IRCServer, cfg.Nickname, cfg.Username, cfg.Channel, nil, false}
+	irc := IRC{"irc.freenode.net:6667", "PugBotTest", "0 0 0 0", "#PugBotTest", nil, false}
 
 	for {
 		if (!irc.connected) {
 			if (irc.ConnectToServer()) {
 				fmt.Println("Connected!")
-				irc.WriteData("NICK %s\r\n", cfg.Nickname);
-				irc.WriteData("USER %s\r\n", cfg.Username);
+				irc.WriteData("NICK %s\r\n", irc.nickname);
+				irc.WriteData("USER %s\r\n", irc.username);
 				for {
-					if (!irc.connected) {
-						break;
-					}
 					irc.RecvData()
 				}
 			} else {
